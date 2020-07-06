888ee42d7f73575b36ba8941987dee16784926cc
unknown <asotjrs@gmail.com>
2020-07-06 20:35:59 +0200

    to memorize some of the types later

--- typescript.js
+++ typescript.js
@@ -1,4 +1,5 @@
-var sum = function (a, b) {
-    return a + b;
-};
-console.log(sum(4, 5));
+var age = 48;
+var me = 'im only ' + age + ' old';
+console.log(me);
+//there are a lot of other examples and types that are available online ;
+// they're sooo simple its just like Java so no problem dealing with them
--- typescript.ts
+++ typescript.ts
@@ -1,4 +1,5 @@
-const sum=(a: number,b: number) =>{
-    return a+b;
-};
-console.log(sum(4,5));
+var age:number=48;
+var me:string='im only '+age+' old';
+console.log(me);
+//there are a lot of other examples and types that are available online ;
+// they're sooo simple its just like Java so no problem dealing with them
